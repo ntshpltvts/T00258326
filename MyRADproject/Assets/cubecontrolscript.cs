e38722429da7033d5774e69703dd6623884923de
STUDENT Nataliia Poltavets <Nataliia.Poltavets@students.ittralee.ie>
2023-09-27 13:06:53 +0100

    fDuplication Signed-off-by: STUDENT Nataliia Poltavets <Nataliia.Poltavets@students.ittralee.ie>

--- MyRADproject/Assets/cubecontrolscript.cs
+++ MyRADproject/Assets/cubecontrolscript.cs
@@ -1,27 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class cubecontrolscript : MonoBehaviour
 {
+    Rigidbody myRB;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 7, 0);
+        myRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) 
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * Time.deltaTime;
+            myRB.AddForce(transform.forward);
+            //transform.position += transform.forward * Time.deltaTime;
+
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRB.AddExplosionForce(10, transform.position + Vector3.down, 5);
+            //transform.position += transform.forward * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.up, -90 * Time.deltaTime);
         }
-
-
     }
 }
